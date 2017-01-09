18cfcf0944640b8568e908485cb095795dbb555e
f1rk0 <f1rk0top1@gmail.com>
2017-01-09 19:17:42 +0300

    first loop with Sheets and Return to Cell

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -5,19 +5,41 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
 	public Text text;
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
+	private States myState;
 
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello, stranger!";
+		myState = States.cell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
-			text.text = "You are in a prison cell, and you want to escape. There are " +
-			"some dirty sheets on the bed, a mirror on the wall, and the door " +
-			"is locked from the outside.\n\n" +
-			"Press S to view Sheets, M to view Mirror or D to view Door";
+		print(myState);
+		if (myState == States.cell){
+			state_cell();
+		} else if (myState == States.sheets_0){
+			state_sheets_0();
+		}
+	}
+	
+	void state_cell (){
+		text.text = "You are in a prison cell, and you want to escape. There are " +
+					"some dirty sheets on the bed, a mirror on the wall, and the door " +
+					"is locked from the outside.\n\n" +
+					"Press S to view Sheets, M to view Mirror or D to view Door";
+		if (Input.GetKeyDown(KeyCode.S)){
+			myState = States.sheets_0;
+		}
+	}
+	
+	void state_sheets_0 (){
+		text.text = "You can't believe you sleep in these things. Surely it's " +
+					"time somebody changed them. The pleasures of prison life " +
+					"I guess!\n\n" +
+					"Press R to Return to roaming your cell";
+		if (Input.GetKeyDown(KeyCode.R)){
+			myState = States.cell;
 		}
 	}
 }
