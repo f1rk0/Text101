82cf0d751e8961937e2fc285940c12d0552195aa
f1rk0 <f1rk0top1@gmail.com>
2017-01-09 18:38:44 +0300

    added story text and hotkey sample

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -8,13 +8,16 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello world!";
+		text.text = "Hello, stranger!";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)){
-			text.text = "Space key pressed";
+			text.text = "You are in a prison cell, and you want to escape. There are " +
+			"some dirty sheets on the bed, a mirror on the wall, and the door " +
+			"is locked from the outside.\n\n" +
+			"Press S to view Sheets, M to view Mirror or D to view Door";
 		}
 	}
 }
