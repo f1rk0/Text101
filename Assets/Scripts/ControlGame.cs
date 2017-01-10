4bee9c03f5e6bab13abe21f0705ff408ff31a7e7
f1rk0 <f1rk0top1@gmail.com>
2017-01-10 17:33:32 +0300

    Update 1.3: Restart and Quit buttons added

---
+++ Assets/Scripts/ControlGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlGame : MonoBehaviour {
+
+	public void ForceQuit(){
+		Application.Quit();
+	}
+	public void ForceStart(string name){
+		Application.LoadLevel(name);
+	}
+}
